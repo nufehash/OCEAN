ba918f7a5c49d09433bde84467abd84a60241155
nufehash <865887390@qq.com>
2024-10-30 10:54:56 +0800

    Update train_OCEAN.m

--- train_OCEAN.m
+++ train_OCEAN.m
@@ -8,15 +8,13 @@ function [BB,XW,YW,HH] = train_OCEAN(XTrain_new,YTrain_new,LTrain_new,GTrain_new
     alpha = param.alpha; 
     beta = param.beta;
     mu= param.mu;
-    %
-  
     nbits = param.nbits;
     n2 = size(LTrain_new,1);
     c = size(LTrain_new,2);
+    max_iterf = 1;
     %initization
     B_new = sign(randn(n2, nbits)); B_new(B_new==0) = -1;
     V_new = randn(n2, nbits);
-
     dX = size(XTrain_new,2);
     dY = size(YTrain_new,2);
     W=randn(c, c);
@@ -24,6 +22,7 @@ function [BB,XW,YW,HH] = train_OCEAN(XTrain_new,YTrain_new,LTrain_new,GTrain_new
     
     for i = 1:max_iter
         %fprintf('iteration %3d\n', i);
+     
         P1=(theta*(HH{1,2}+XTrain_new'*XTrain_new)+delta*eye(dX))\(theta*(HH{1,1}+XTrain_new'*B_new));
         P2=(theta*(HH{1,17}+YTrain_new'*YTrain_new)+delta*eye(dY))\(theta*(HH{1,16}+YTrain_new'*B_new));
         Wtemp1=(beta+alpha)*(HH{1,4}+LTrain_new'*LTrain_new)+ delta*eye(c);
@@ -31,8 +30,7 @@ function [BB,XW,YW,HH] = train_OCEAN(XTrain_new,YTrain_new,LTrain_new,GTrain_new
         W=Wtemp1\Wtemp2;
         G=(beta*(HH{1,15}+V_new'*V_new)+delta*eye(nbits))\(HH{1,5}+V_new'*(LTrain_new*W));
         Z = B_new+beta*LTrain_new*W*G'...
-            +mu*nbits*GTrain_new*(HH{1,6}+GTrain_new'*B_new);
-        
+            +mu*nbits*GTrain_new*(HH{1,6}+GTrain_new'*B_new);   
         Temp = Z'*Z-1/n2*(Z'*ones(n2,1)*(ones(1,n2)*Z));
         [~,Lmd,QQ] = svd(Temp); clear Temp
         idx = (diag(Lmd)>1e-6);
@@ -42,7 +40,6 @@ function [BB,XW,YW,HH] = train_OCEAN(XTrain_new,YTrain_new,LTrain_new,GTrain_new
         V_new = sqrt(n2)*[P P_]*[Q Q_]';
         B_new = sign(theta*XTrain_new*P1+theta*YTrain_new*P2+V_new...
                      +mu*nbits*GTrain_new*(HH{1,7}+GTrain_new'*V_new));
-
     end
     
     H1_new = XTrain_new'*B_new;
@@ -91,35 +88,24 @@ function [BB,XW,YW,HH] = train_OCEAN(XTrain_new,YTrain_new,LTrain_new,GTrain_new
 %         /(eye(nbits)+HH{1,8}*xi);
 %     YW = (HH{1,14}+lambda*eye(dY))\(HH{1,12}+HH{1,10}*xi*nbits)...
 %         /(eye(nbits)+HH{1,8}*xi);
-
-
-    
-     %%%hash function learning
-         %
          M1 = zeros(size(B_new));
          M2 = M1;
-         for iter = 1:max_iter
+         for iter = 1:max_iterf
              T1 = B_new + B_new.*M1;
              T2 = B_new + B_new.*M2;
-             
              F1 = (HH{1,9}+XTrain_new'*XTrain_new+param.xi*eye(size(XTrain_new,2)))    \    (HH{1,11}+XTrain_new'*T1);
              F2 = (HH{1,10}+YTrain_new'*YTrain_new+param.xi*eye(size(YTrain_new,2)))    \    (HH{1,12}+YTrain_new'*T2);
-             
              K1 = XTrain_new* F1-B_new;
              K2 = YTrain_new* F2-B_new;
-             
              M1 = max(K1.*B_new,0);
              M2 = max(K2.*B_new,0);
          end
          XW=F1;
          YW=F2;
-    
          H9_new = XTrain_new'*XTrain_new;
          H10_new = YTrain_new'*YTrain_new;
          H11_new = XTrain_new'*T1;
          H12_new = YTrain_new'*T2;
-         
-         
          HH{1,9} = HH{1,9}+H9_new;
          HH{1,10} = HH{1,10}+H10_new;
          HH{1,11} = HH{1,11}+H11_new;
