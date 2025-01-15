7e740976cf32af2b5123bd13970a0aa6ce94a8f1
nufehash <865887390@qq.com>
2025-01-15 15:35:02 +0800

    Update main_demo.m

--- main_demo.m
+++ main_demo.m
@@ -1,21 +1,22 @@
 close all; clear; clc;
-db = {'IAPRTC-12'};
+db = {'IAPRTC-12'};%%  load dataset
 hashmethods = {'OCEAN'};
-param.nbits = 16;
-param.top_K = 2000;
-[XChunk,YChunk,LChunk,XTest,YTest,LTest,chunks] = load_data(db);
+%%  initialization
+param.nbits = 16;   %% Hash codes length setting
+param.top_K = 2000; %% The top K(2000)results with the highest similarity.
+[XChunk,YChunk,LChunk,XTest,YTest,LTest,chunks] = load_data(db);%% Load training dataset
 fprintf('========%s %d bits start======== \n', 'OCEAN',param.nbits);
 OCEANparam = param;
-OCEANparam.nchunks=chunks;
+OCEANparam.nchunks=chunks;%% Number of data chunks
+%% Parameter setting
 OCEANparam.theta=0.5;
 OCEANparam.alpha=1e4;
-OCEANparam.beta=1e0; 
+OCEANparam.beta=1e0;
 OCEANparam.mu=1e5;
+OCEANparam.omega=1e0;
+%%
+%% --------------------OURS----------------------------%%
 eva_info_ = evaluate_OCEAN(XChunk,YChunk,LChunk,XTest,YTest,LTest,OCEANparam);
 fprintf('Image_VS_Text_MAP: %6.4f.\n', eva_info_{9}.Image_VS_Text_MAP);
-% evaluation_info.Image_VS_Text_MAP50 = mAP(orderH', LTrain, LTest, 50);
-fprintf('Image_VS_Text_MAP100: %6.4f.\n', eva_info_{9}.Image_VS_Text_MAP100);
 fprintf('Text_VS_Image_MAP: %6.4f.\n', eva_info_{9}.Text_VS_Image_MAP);
-% evaluation_info.Text_VS_Image_MAP50 = mAP(orderH', LTrain, LTest, 50);
-fprintf('Text_VS_Image_MAP100: %6.4f.\n', eva_info_{9}.Text_VS_Image_MAP100);
 
