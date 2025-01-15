c3b750dbddffc3bf6bb919efe77b2e8225b50c0d
nufehash <865887390@qq.com>
2025-01-15 15:35:33 +0800

    Update precision_recall.m

--- precision_recall.m
+++ precision_recall.m
@@ -1,10 +1,29 @@
 function [precision, recall] = precision_recall(ids, Lbase, Lquery)
-
+%
+%This function precision_recall calculates the precision and recall curves for a given set of query results. 
+% Precision and recall are common metrics used to evaluate the performance of information retrieval systems.
+%[precision, recall] = precision_recall(ids, Lbase, Lquery);
+  %Input
+  % ids: A matrix where each column represents the ranked list of indices of the base dataset for a particular query.
+  % Lbase: A matrix of labels for the base dataset.
+  % Lquery: A matrix of labels for the query dataset.
+  % Output
+  % precision: A vector representing the precision values at different recall levels.
+  % recall: A vector representing the recall levels.
+  %
+%%
+% % Reference:
+% Online semantic embedding correlation for discrete cross-media hashing. 
+% (Manuscript)
+% Version1.0 -- Jan/2025
+% Contant: Haoyu Hu (982258029@qq.com)
+%
+% Initialize variables
 nquery = size(ids, 2);
 K = size(ids, 1);
 P = zeros(K, nquery);
 R = zeros(K, nquery);
-
+% Calculate precision and recall for each query
 for i = 1 : nquery
     label = Lquery(i, :);
     label(label == 0) = -1;
@@ -18,11 +37,12 @@ for i = 1 : nquery
     P(:, i) = Lk ./ (1:K)';
     R(:, i) = Lk ./ LK;
 end
+% Calculate the mean precision and recall
 mP = mean(P, 2);
 mR = mean(R, 2);
 mP = [mP(1); mP];
 mR = [0; mR];
-
+% Interpolate precision values for a range of recall levels
 recall = (0.0:0.001:max(mR))';
 precision = interpolate_pr(mR, mP, recall)';
 
@@ -30,12 +50,20 @@ end
 
 
 function precision = interpolate_pr(r, p, recs)
-
+%
+% Input
+% r: Recall values.
+% p: Precision values.
+% recs: Recall levels for which to interpolate precision values.
+% Output
+% precision: Interpolated precision values.
+%
+% Check input sizes
 n = numel(p);
 if (n ~= numel(r))
     error('two first arguments should be of the same size');
 end
-
+%Interpolate precision for each recall level
 for j = 1:numel(recs)
     rec = recs(j);
     done = 0;
